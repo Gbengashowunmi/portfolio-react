a550c4e5034c76bb7a4dc5aaeed69c9d2b2d11cb
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-01-07 10:29:58 +0100

    typo error

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,11 +4,22 @@ import Project from "./Project";
 import { motion } from "framer-motion";
 import AOS from 'aos';
 import "aos/dist/aos.css";
+import { BsGithub } from "react-icons/bs";
+import { FaReact, FaSass } from "react-icons/fa";
+import { IoLogoCss3, IoLogoHtml5, IoLogoJavascript } from "react-icons/io5";
+import { TbBrandNextjs } from "react-icons/tb";
 
 AOS.init();
 
 
 export default function Portfolio() {
+  const icongit = <BsGithub />
+  const iconReact = <FaReact/>
+  const iconCSS = <IoLogoCss3/>
+  const iconNext = <TbBrandNextjs/>
+  const iconjs = <IoLogoJavascript/>
+  const iconHtml = <IoLogoHtml5/>
+  const iconSass = <FaSass/>
   return (
     <motion.div
       className="container text-center  bg-black"
@@ -44,24 +55,43 @@ export default function Portfolio() {
             img="/images/bank-app.jpg"
             title="Banking Web App"
             description="This is a website built for online banking transaction where you can log in, check your blance, take a loan, transfer money to another existing customer, make withdrawals and also close your account. "
+            iconjs={iconjs}
+            iconCss={iconCSS}
+            iconHtml={iconHtml}
+            icongit={icongit}
+            
           />
           <Project
             link="//e-blog-next.vercel.app/"
             img="/images/e-blog-next.jpg"
-            title="News Website (Reactjs)"
+            title="News Website (NextJS)"
             description="This is a news/Blog website that displays latest news around the globe. you can visit to read news, create account to comment and also publish post that'll be verified by an admin before actual publishing.  "
+            icongit={icongit}
+            iconNext={iconNext}
+            iconSass ={iconSass}
+
           />
           <Project
             link="//e-blog-news.vercel.app/"
             img="/images/e-blog-react.jpg"
-            title="News Website (NextJS)"
+            title="News Website (Reactjs)"
             description="This is a news/Blog website that displays latest news around the globe. you can visit to read news, create account to comment and also publish post that'll be verified by an admin before actual publishing.  "
+            icongit={icongit}
+            iconReact={iconReact}
+            iconSass ={iconSass}
+            
+
           />
           <Project
             link="//gbengashowunmi.github.io/guessing/"
             img="/images/guess.jpg"
             title="Guessing game"
             description="This is a simple game app where you guess a number and get a increase in score for correct answer and decrease in score for a wrong answer "
+            icongit={icongit}
+              iconjs={iconjs}
+            iconCss={iconCSS}
+            iconHtml={iconHtml}
+
           />
         </div>
         <div className="navigation">
--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Project({img, link, title, description}) {
+export default function Project({img, link, title, description, iconSass, iconReact,iconjs,iconNext, icongit, iconCss, iconHtml}) {
   return (
     <div className="project">
       <div className="overlay">
@@ -10,6 +10,15 @@ export default function Project({img, link, title, description}) {
         </div>
       <img src={img} alt="project" />
       <div className="caption"> <h3>{title}</h3><p> {description}</p></div>
+      <div className="tools">  
+      {iconHtml && iconHtml}
+      {iconCss && iconCss}
+      {iconSass && iconSass}
+      {iconjs && iconjs}
+      {iconReact && iconReact}
+      {iconNext && iconNext}
+      {icongit && icongit}
+      </div>
     </div>
   );
 }
